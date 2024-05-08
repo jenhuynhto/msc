d5c08d0c4afdee4ad1dda41f39f9e696a0749761
jenhuynhto <jehto@ucsc.edu>
2024-05-07 17:44:13 -0700

    Refactor Nav.jsx Links and add image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Nav from "./components/Nav";
 
-
 function App() {
 
   return (
     <>
-      <div className="bg-slate-600">
-        <Nav/>
+      <Nav />
+      <div className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-screen">
       </div>
     </>
   )
